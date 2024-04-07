05065396c88b0a24c9f0b1f99c600eb38899d830
Meagan Welch <107885378+welchmea@users.noreply.github.com>
2024-04-06 22:22:21 -0500

    Update Jobs_has_Departments.py

--- blueprints/jobDept/Jobs_has_Departments.py
+++ blueprints/jobDept/Jobs_has_Departments.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect
 import os
+import psycopg2
 
 jobdepartment = Blueprint('jobDept', __name__,)
 
@@ -8,7 +9,8 @@ jobdepartment = Blueprint('jobDept', __name__,)
 #     conn = psycopg2.connect(host='ec2-3-232-218-211.compute-1.amazonaws.com', database='d6af5e5pibqrf1', user='xbeltbfqliosyk', password='294e9b67571b4e7e9ed12a10d8f0a5591750f681382b0a8193eeece39e5fde68')
 #     return conn
 
-conn = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # conn = get_connection()
 # ----------------------------------------
