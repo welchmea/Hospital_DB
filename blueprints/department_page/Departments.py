ef8e8ec8e6c66862690fef2e932491e832216f80
Meagan <welchmea@oregonstate.edu>
2024-04-09 16:37:58 -0500

    fixed styling

--- blueprints/department_page/Departments.py
+++ blueprints/department_page/Departments.py
@@ -1,29 +1,29 @@
 from flask import Blueprint, render_template, request, redirect
 import psycopg2
 import os
-from config import load_config
+# from config import load_config
 
 emp_department = Blueprint('department_page', __name__)
 
 # -----------------------------
 # --- CRUD for Departments ----
 # -----------------------------
-def connect(config):
-    """ Connect to the PostgreSQL database server """
-    try:
-        # connecting to the PostgreSQL server
-        with psycopg2.connect(**config) as conn:
-            print('Connected to the PostgreSQL server.')
-            return conn
-    except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
+# def connect(config):
+#     """ Connect to the PostgreSQL database server """
+#     try:
+#         # connecting to the PostgreSQL server
+#         with psycopg2.connect(**config) as conn:
+#             print('Connected to the PostgreSQL server.')
+#             return conn
+#     except (psycopg2.DatabaseError, Exception) as error:
+#         print(error)
         
-# DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-config = load_config()
-conn = connect(config)
+# config = load_config()
+# conn = connect(config)
 
 # route for departments page
 @emp_department.route("/departments", methods=["POST", "GET"])
