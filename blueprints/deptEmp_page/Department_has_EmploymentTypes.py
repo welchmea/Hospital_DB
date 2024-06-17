366b68fcfdc880758b453dfba033891e539269d1
Meagan <welchmea@oregonstate.edu>
2024-06-17 14:21:28 -0500

    changed all naming to match PEP standards

--- blueprints/deptEmp_page/Department_has_EmploymentTypes.py
+++ blueprints/deptEmp_page/Department_has_EmploymentTypes.py
@@ -15,8 +15,10 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 departmentID, typeName, dep_empTypes_data = None,None,None
 
 
-@dept_emp.route("/dep_empTypes", methods=["GET"])
-def dep_empTypes():
+@dept_emp.route("/dep_emptypes", methods=["GET"])
+def dep_emptypes():
+    
+    dep_emptypes_data = None
     # Grabs Department has EmploymentTypes data from mySQl and call template to display
     if request.method == "GET":
         # mySQL query to grab all from Department has EmploymentTypes data
@@ -30,48 +32,50 @@ def dep_empTypes():
             ON Departments.departmentID = Department_has_EmploymentTypes.departmentID;"
         cur = conn.cursor()
         cur.execute(query)
-        dep_empTypes_data = cur.fetchall()
+        dep_emptypes_data = cur.fetchall()
 
-    return render_template("dep_empTypes.j2", dep_empTypes_data=dep_empTypes_data)
+    return render_template("dep_empTypes.j2", dep_emptypes_data=dep_emptypes_data)
 
 
 # Route for delete functionality, deleting selected Department EmploymentType by empDeptID
-@dept_emp.route("/delete_dep_empTypes/<int:empDeptID>")
-def delete_dep_empTypes(empDeptID):
+@dept_emp.route("/delete_dep_emptypes/<int:empDeptID>")
+def delete_dep_emptypes(empdeptid):
     # mySQL query to delete the schedule with passed id
     query = "DELETE FROM Department_has_EmploymentTypes WHERE empDeptID = '%s';"
     cur = conn.cursor()
-    cur.execute(query, [empDeptID])
+    cur.execute(query, [empdeptid])
     conn.commit()
 
     # Redirect back to schedules page
-    return redirect("/dep_empTypes")
+    return redirect("/dep_emptypes")
 
 
 # Route for add functionality, adds the attributes of a department employmentType
-@dept_emp.route("/add_dep_empTypes", methods=["POST", "GET"])
-def add_dep_empTypes():
+@dept_emp.route("/add_dep_emptypes", methods=["POST", "GET"])
+def add_dep_emptypes():
+    
+    typename, departmentid, dep_emptypes_data = None, None, None
     # Inserts data about a new schedule into the schedules entity
     if request.method == "POST":
         # grab user form inputs
         if request.form.get("add_dep_empTypes"):
-            typeName = request.form["typeName"]             # FK
-            departmentID = request.form["departmentID"]     # FK
+            typename = request.form["typeName"]             # FK
+            departmentid = request.form["departmentID"]     # FK
 
         # Account for null typeName
-        if typeName is None:
+        if typename is None:
             query = "INSERT INTO Department_has_EmploymentTypes (departmentID) \
              VALUES((SELECT departmentID FROM Departments WHERE depName = %s));"
             cur = conn.cursor()
-            cur.execute(query, departmentID)
+            cur.execute(query, departmentid)
             conn.commit()
 
         # Account for null departmentID
-        if departmentID is None:
+        if departmentid is None:
             query = "INSERT INTO Department_has_EmploymentTypes (typeName) \
             VALUES((SELECT typeName FROM EmploymentTypes WHERE typeName = %s));"
             cur = conn.cursor()
-            cur.execute(query, typeName)
+            cur.execute(query, typename)
             conn.commit()
 
         # No null inputs
@@ -80,27 +84,27 @@ def add_dep_empTypes():
             VALUES((SELECT departmentID FROM Departments WHERE depName = %s),\
             (SELECT typeName FROM EmploymentTypes WHERE typeName = %s));"
             cur = conn.cursor()
-            cur.execute(query, (departmentID, typeName))
+            cur.execute(query, (departmentid, typename))
             conn.commit()
 
         # Redirect back to Department Has EmploymentTypes page after executing the add query
-        return redirect("/dep_empTypes")
+        return redirect("/dep_emptypes")
 
     # Button click renders new page to add EmpType/Department Data
     if request.method == "GET":
         query = "SELECT * FROM Department_has_EmploymentTypes"
         cur = conn.cursor()
         cur.execute(query)
-        dep_empTypes_data = cur.fetchall()
+        dep_emptypes_data = cur.fetchall()
         query2 = "SELECT departmentID, depName FROM Departments"
         cur = conn.cursor()
         cur.execute(query2)
-        departmentFK = cur.fetchall()
+        departmentfk = cur.fetchall()
         query3 = "SELECT typeName FROM EmploymentTypes"
         cur = conn.cursor()
         cur.execute(query3)
-        empTypeFK = cur.fetchall()
-        return render_template("add_dep_empTypes.j2",
-                               dep_empTypes_data=dep_empTypes_data,
-                               departmentFK=departmentFK,
-                               empTypeFK=empTypeFK)
+        emptypefk = cur.fetchall()
+        return render_template("add_dep_emptypes.j2",
+                               dep_emptypes_data=dep_emptypes_data,
+                               departmentfk=departmentfk,
+                               emptypefk=emptypefk)
